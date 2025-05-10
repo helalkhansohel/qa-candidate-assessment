33ead33895eec0573e21ac55997a5c4ef98c0901
Helal <md.helalkhansohel@gmail.com>
2025-05-10 16:14:27 +0600

    Updated/CI-CD pipeline for performance testing v119

--- performance-tests/config/scenarios/spike.js
+++ performance-tests/config/scenarios/spike.js
@@ -2,7 +2,7 @@ export const spikeScenarioConfiguration = {
     executor: 'ramping-vus',
     startVUs: 0,
     stages: [
-        { duration: '30s', target: 5 }, //1m-10
+        { duration: '30s', target: 5 }, //1m -10
         { duration: '10s', target: 20 }, //10s -100
         { duration: '30s', target: 3 } //1m - 10
     ],
--- performance-tests/config/thresholds.js
+++ performance-tests/config/thresholds.js
@@ -1,7 +1,7 @@
 export const orderThresholds = {
-    http_req_duration: ['p(50)<1000', 'p(70)<3000'], //95-500, 99-1000
-    http_req_failed: ['rate<0.30'], //.01
-    checks: ['rate>0.90'] //99
+    http_req_duration: ['p(50)<1000', 'p(70)<3000'], //Should be 95-500, 99-1000
+    http_req_failed: ['rate<0.30'], //Should be .01
+    checks: ['rate>0.90'] //Should be 0.99
 };
 
 // export const authThresholds = {
